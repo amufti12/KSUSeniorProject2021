8c57e62e406586f6ca82870037732e36038166ea
EricMo1016 <43613366+EricMo1016@users.noreply.github.com>
2020-10-19 18:03:16 -0400

    latest functional web portal

--- WebPortal/js/apirequest.js
+++ WebPortal/js/apirequest.js
@@ -4,33 +4,59 @@ function myApiRequestFunction()
 	{
 	// Create a request variable and assign a new XMLHttpRequest object to it.
 	var request = new XMLHttpRequest();
-	var url = "http://a2b538e9a13274218be989a87af346ab-1215678508.us-east-1.elb.amazonaws.com:8080/validateUser?username=jdoe1&password=password";
 	var loginStatus;
-	//var user = document.getElementById('username')
-	//var pass
+	var user = document.getElementById('usernameID').value;
+	var pass = document.getElementById('passwordID').value;
+	var url = "http://a2b538e9a13274218be989a87af346ab-1215678508.us-east-1.elb.amazonaws.com:8080/validateUser?username=" + user + "&password=" + pass;
 
 	// Open a new connection, using the GET request on the URL endpoint
 	request.open('GET', url, true);
 	console.log('a');
 
-	request.onload = function () {
-  		// Begin accessing JSON data here
-		var data = JSON.parse(this.response);
 	
-		if (request.status >= 200 && request.status < 400) {
-  			loginStatus = data.successfulLogin;
-  		}
-		else {
-  			console.log('error');
-		}		
-	}
 
 	// Send request
 	request.send();
 	console.log('b');
 	
-	if(loginStatus)
- 		location.replace("providerview.html");
-  	else
-  		location.replace("loginfailview.html");
+	request.onload = () => {
+  		// Begin accessing JSON data here
+  		console.log(request);
+		var data = JSON.parse(request.response);
+	
+		if (request.status >= 200 && request.status < 400) {
+  			loginStatus = data.successfulLogin;
+  			console.log(loginStatus);
+  		}
+		else {
+			location.replace("loginfailview.html")
+  			console.log('error');
+		}
+		
+		console.log(user);
+		
+		if(loginStatus)
+		{
+			if(user == 'jdoe1')
+			{
+				location.replace("providerview.html");
+			}
+  			if(user == 'james2')
+  			{
+  				location.replace("patientview.html");
+  			}
+  			if(user != 'jdoe1' && user != 'james2')
+  			{
+  				location.replace("loginfailview.html");
+  			}
+  		}
+  		else
+  		{
+  			location.replace("loginfailview.html");
+  		}
+  					
+	}
+	
+	console.log(loginStatus);
+	
   }
